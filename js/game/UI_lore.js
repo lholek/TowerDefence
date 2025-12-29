fe026fa7abb5e64c9ae09e64d15a1b7bd572844f
lholek <holeklukas@gmail.com>
2025-12-29 23:55:04 +0100

    + lore

---
+++ js/game/UI_lore.js
@@ -0,0 +1,20 @@
+const loreBtn = document.getElementById('openLoreBtn');
+const lorePopup = document.getElementById('lorePopup');
+const closeLore = document.getElementById('closeLore');
+
+// Open Lore
+loreBtn.onclick = function() {
+  lorePopup.style.display = 'flex';
+}
+
+// Close Lore
+closeLore.onclick = function() {
+  lorePopup.style.display = 'none';
+}
+
+// Close if user clicks outside the parchment
+window.onclick = function(event) {
+  if (event.target == lorePopup) {
+    lorePopup.style.display = 'none';
+  }
+}
