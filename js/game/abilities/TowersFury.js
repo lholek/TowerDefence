44c067e196ab10beb5c41266444003a30f55efa5
lholek <holeklukas@gmail.com>
2026-01-04 18:23:01 +0100

    + Multiple abilites in editor and game working (both towers_fury and lava_floor)

--- js/game/abilities/TowersFury.js
+++ js/game/abilities/TowersFury.js
@@ -5,10 +5,10 @@ export default class TowersFury extends Ability {
     constructor(game, config = {}) {
         super(game, config);
         
-        // FIX: Look for 'config_id' from your JSON and assign it to this.id
+        // FIX: Look for 'configId' from your JSON and assign it to this.id
         // This ensures Fury 1 and Fury 2 are recognized as different objects.
-        if (config.config_id) {
-            this.id = config.config_id;
+        if (config.configId) {
+            this.id = config.configId;
         }
 
         this.type = 'global';
